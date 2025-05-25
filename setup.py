bcd96b1fced461861922856e94fcbf17a6abe479
Abbi :0 <166131718+Tiredabee@users.noreply.github.com>
2025-05-25 14:21:07 -0400

    Update setup.py

--- setup.py
+++ setup.py
@@ -5,22 +5,6 @@ from setuptools import setup, find_packages, Extension  # This setup relies on s
 from setuptools.command.build_ext import build_ext as _build_ext
 import numpy
 
-extensions = [
-    Extension(
-        name="pylandau",  # The module name you import
-        sources=[
-            "pyLandau/cpp/pylandau.pyx",     # Your Cython file (renamed from .cpp to .pyx?)
-            "pyLandau/cpp/pylandau_src.cpp", # Your C++ implementation
-        ],
-        include_dirs=[
-            numpy.get_include(),             # NumPy headers
-            "pyLandau/cpp",                  # Your custom headers
-        ],
-        language="c++",
-        extra_compile_args=["-std=c++11"],
-    )
-]
-
 class build_ext(_build_ext):
     def finalize_options(self):
         _build_ext.finalize_options(self)
@@ -28,7 +12,7 @@ class build_ext(_build_ext):
         builtins.__NUMPY_SETUP__ = False
         import numpy
         self.include_dirs.append(numpy.get_include())
-
+        
 # Determine platform-specific compiler flags
 extra_compile_args = ["-std=c++11"]  # Or use -std=c++17 if you like
 extra_link_args = []
@@ -45,17 +29,31 @@ try:
     have_cython = True
 except ImportError:
     pass
-
 if have_cython:
-    cpp_extension = cythonize(Extension('pylandau', ['pyLandau/cpp/pylandau.pyx'],
-        extra_compile_args=extra_compile_args,
-        extra_link_args=extra_link_args,))
+    # Use cythonize on .pyx, pass compiler and linker flags
+    cpp_extension = cythonize(
+        Extension(
+            'pylandau',
+            sources=['pyLandau/cpp/pylandau.pyx', 'pyLandau/cpp/pylandau_src.cpp'],  # Added missing cpp source!
+            language="c++",
+            include_dirs=[numpy.get_include(), "pyLandau/cpp"],
+            extra_compile_args=extra_compile_args,
+            extra_link_args=extra_link_args,
+        )
+    )
 else:
-    cpp_extension = [Extension('pylandau',
-                               sources=['pyLandau/cpp/pylandau.cpp'],
-                               language="c++",
-        extra_compile_args=extra_compile_args,
-        extra_link_args=extra_link_args,)]
+    # Fallback: compile from .cpp only
+    cpp_extension = [
+        Extension(
+            'pylandau',
+            sources=['pyLandau/cpp/pylandau.cpp', 'pyLandau/cpp/pylandau_src.cpp'],  # Added pylandau_src.cpp here too
+            language="c++",
+            include_dirs=[numpy.get_include(), "pyLandau/cpp"],
+            extra_compile_args=extra_compile_args,
+            extra_link_args=extra_link_args,
+        )
+    ]
+
 
 install_requires = ['cython>=0.29', 'numpy>=1.21']
 
