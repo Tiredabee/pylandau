44aef85ac0a21dcbea8d850cc094b6753ffdfdd6
Abbi :0 <166131718+Tiredabee@users.noreply.github.com>
2025-05-25 14:06:52 -0400

    Update pylandau_src.h

--- pyLandau/cpp/pylandau_src.h
+++ pyLandau/cpp/pylandau_src.h
@@ -1,1 +1,10 @@
+#ifndef PYLANDAU_SRC_H
+#define PYLANDAU_SRC_H
 
+double * getLandauPDFData(double * data, const unsigned int size, const double mu, const double eta);
+double * getLangauPDFData(double * data, const unsigned int size, const double mu, const double eta, const double sigma);
+double landauPDF(const double x, const double xi, const double x0);
+double landauGaussPDF(const double x, const double mu, const double eta, const double sigma);
+double gaussPDF(const double x, const double mu, const double sigma);
+
+#endif // PYLANDAU_SRC_H
